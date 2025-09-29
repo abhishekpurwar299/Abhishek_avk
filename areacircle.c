a574e340bc4feed81ef254d1db25b657ff979d0c
Abhishek Purwar <abhishekpurwar299@gmail.com>
2025-09-30 00:13:02 +0530

    Add files via upload

---
+++ areacircle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    float rad, area;
+    printf("enter radius of circle");
+    scanf("%f",&rad);
+    area = 3.14 * rad * rad;
+    printf("area is : %f",area);
+    return 0;
+
+}
